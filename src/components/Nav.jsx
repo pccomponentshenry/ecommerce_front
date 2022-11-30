2d0c442d2bdb471fd7a86739b37b9c3cd63d197b
SantiCalabro <santi.calabro93@gmail.com>
2022-11-30 02:02:01 -0300

    cambios en el nav y en el home

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,22 +6,28 @@ import cart from "../Images/cart.png";
 export default function Nav() {
   return (
     <>
-      <div className={N.navbarCont}>
-        <h1>Logo</h1>
-        <ul className={N.navList}>
-          <li>Categories</li>
-          <li>Latest</li>
-          <li>Sell</li>
-        </ul>
-      </div>
+      <div className={N.container}>
+        <div className={N.logoAndMenu}>
+          <h1>Logo</h1>
+          <ul className={N.navList}>
+            <li>Categories</li>
+            <li>Latest</li>
+            <li>Sell</li>
+          </ul>
+        </div>
 
-      <div className={N.searchBarCont}>
-        <input type="text" placeholder="Search" />
-        <img src={search} alt="search icon" className={N.searchIcon} />
+        <div className={N.searchAndCart}>
+          <div className={N.searchBar}>
+            <input type="text" placeholder="Search" />
+            <img src={search} alt="search icon" className={N.searchIcon} />
+          </div>
+          <h6 className={N.loginText}>Login/Register</h6>
+          <img src={cart} className={N.cart} alt="cart icon" />
+        </div>
+        <div className={N.line}>
+          <hr />
+        </div>
       </div>
-      <h6 className={N.loginText}>Login/Register</h6>
-      <img src={cart} className={N.cart} alt="cart icon" />
-      <hr />
     </>
   );
 }
