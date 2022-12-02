f6a7e3db8621395ca71d350346319cc5adf3d428
Diego Cubillos <dcubillos12@hotmail.com>
2022-12-01 19:51:00 -0500

    feat(reducer/index):add
    GET_PRODUCT
    SEARCH_PRODUCT
    ORDER_BY_NAME

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,9 +1,44 @@
-import {} from "../actions/index"; //Para las action creators
+//import {} from "../actions/index"; //Para las action creators
 
-const initialState = {};
+const initialState = {
+  product: [],
+  allProduct: [],
+};
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
+    case "GET_PRODUCT":
+      return {
+        ...state,
+        product: action.payload,
+        allProduct: action.payload,
+      };
+    case "SEARCH_PRODUCT":
+      return {
+        ...state,
+        product: action.payload,
+      };
+    case "ORDER_BY_NAME":
+      let sortedArr =
+        action.payload === "asc"
+          ? state.product.sort(function (a, b) {
+              if (a.name > b.name) {
+                return 1;
+              }
+              if (b.name > a.name) {
+                return -1;
+              }
+              return 0;
+            })
+          : state.product.sort(function (a, b) {
+              if (a.name > b.name) {
+                return -1;
+              }
+              if (a.name > b.name) {
+                return 1;
+              }
+              return 0;
+            });
   }
 }
 
