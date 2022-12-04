14cb47237ed86227c6e4bf6ed265ef1fbfc79681
RBurgues <rsebaburgues@gmail.com>
2022-12-04 20:10:40 -0300

    Productos desde la DB en el front y selectores con datos desde la db

--- src/redux/actions/actionNames.js
+++ src/redux/actions/actionNames.js
@@ -3,4 +3,5 @@ export const SEARCH_PRODUCT = "SEARCH_PRODUCT";
 export const ORDER_BY_NAME = "ORDER_BY_NAME";
 export const GET_BRANDS = "GET_BRANDS";
 export const GET_PRODUCTS = "GET_PRODUCTS";
+export const GET_CATEGORIES = "GET_CATEGORIES";
 export const ALL_PRODUCTS = "ALL_PRODUCTS";
