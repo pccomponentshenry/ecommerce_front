40e3d4d739478ec6db1a3662374606bce7695237
carlamadeo <carlamadeo@gmail.com>
2022-12-04 14:30:58 -0300

    Database population on start

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,18 +1,24 @@
 //const url = "http://localhost:3001";
 import axios from "axios";
 import {
-//   GET_PRODUCT,
-//   SEARCH_PRODUCT,
-//   ORDER_BY_NAME,
-  GET_BRAND
+  //   GET_PRODUCT,
+  //   SEARCH_PRODUCT,
+  //   ORDER_BY_NAME,
+  GET_BRAND, GET_PRODUCTS
 } from "../actions/actionNames";
+
+const URL = "http://localhost:3001";
 
 export function getBrand() {
   return async (dispatch) => {
-    const res = await axios.get("http://localhost:3001/brands");
-    console.log(res.data);
-
-    
+    const res = await axios.get(`${URL}/brands`);
     return dispatch({ type: GET_BRAND, payload: res.data });
+  };
+}
+
+export function getProducts() {
+  return async (dispatch) => {
+    const res = await axios.get(`${URL}/products`);
+    return dispatch({ type: GET_PRODUCTS, payload: res.data });
   };
 }
