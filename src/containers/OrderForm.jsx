7c813a90e93498331ceddb212e3936c202abb654
SantiCalabro <santi.calabro93@gmail.com>
2022-12-28 14:17:08 -0300

    order

--- src/containers/OrderForm.jsx
+++ src/containers/OrderForm.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import O from "../styles/OrderForm.module.css";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export default function OrderForm() {
+  const [input, setInput] = useState({
+    streetName: "",
+    streetNumber: "",
+    apartment: "",
+    zipCode: "",
+    location: "",
+    aditionalInformation: "",
+  });
+  const [error, setError] = useState();
+  const handleChange = e => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const errorSetting = e => {
+    setError(
+      handleValidate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
+  const handleValidate = input => {
+    const errors = {};
+    const regexNumber = /^[0-9]+$/;
+    if (!input.streetName) {
+      errors.streetName = "*Street name is required";
+    }
+    if (!input.streetNumber) {
+      errors.streetNumber = "*Street number is required";
+    } else if (!regexNumber.test(input.streetNumber)) {
+      errors.streetNumber = "*Street number must be an integer number";
+    }
+    if (!input.apartment) {
+      errors.apartment = "*Apartment is required";
+    }
+    if (!input.zipCode) {
+      errors.zipCode = "*Zip code is required";
+    }
+    if (!input.location) {
+      errors.location = "*Location is required";
+    }
+  };
+
   const cart = useSelector(state => state.cart);
   const address = [
     {
@@ -41,6 +86,7 @@ export default function OrderForm() {
   ];
   return (
     <div className={O.container}>
+      <div className={O.darkNav}></div>
       <div className={O.cartContainer}>
         <h1>Shopping cart</h1>
         {cart.map((el, i) => (
@@ -76,18 +122,55 @@ export default function OrderForm() {
       )}
 
       <div className={O.newAddressCont}>
-        {/* <h1>Add a new address</h1> */}
+        <h1>Add a new address</h1>
         <div className={O.formContainer}>
           <form>
-            <input type="text" placeholder="Street name" />
-            <input type="number" placeholder="Street number" />
-            <input type="text" placeholder="Apartment" />
-            <input type="zipCode" placeholder="Zip Code" />
+            <input
+              type="text"
+              name="streetName"
+              value={input.streetName || ""}
+              placeholder="Street name"
+              onChange={e => handleChange(e)}
+              onBlur={e => errorSetting(e)}
+            />
+            <input
+              type="number"
+              name="streetNumber"
+              value={input.streetNumber || ""}
+              placeholder="Street number"
+              onChange={e => handleChange(e)}
+              onBlur={e => errorSetting(e)}
+            />
+            <input
+              type="text"
+              name="apartment"
+              value={input.apartment || ""}
+              placeholder="Apartment"
+              onChange={e => handleChange(e)}
+              onBlur={e => errorSetting(e)}
+            />
+            <input
+              type="zipCode"
+              name="zipCode"
+              value={input.zipCode || ""}
+              placeholder="Zip Code"
+              onChange={e => handleChange(e)}
+              onBlur={e => errorSetting(e)}
+            />
             <select name="location" id="location">
-              <option value="">Default</option>
+              <option value="">Location</option>
             </select>
-            <textarea name="additional" id="" cols="30" rows="10" />
-            <button type="submit">Pay my products</button>
+            <textarea
+              name="additionalInformation"
+              value={input.aditionalInformation || ""}
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Additional information"
+              onChange={e => handleChange(e)}
+              onBlur={e => errorSetting(e)}
+            />
+            <button type="submit">Submit address</button>
           </form>
         </div>
       </div>
