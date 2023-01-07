06086858486ecb8358e050b12705dbc578b67db0
SantiCalabro <santi.calabro93@gmail.com>
2023-01-07 12:12:25 -0300

    for sale

--- src/components/ForSale.jsx
+++ src/components/ForSale.jsx
@@ -28,33 +28,41 @@ export default function ForSale() {
         productsForSale.map((el, i) => (
           <div className={S.cardContainer} key={i}>
             <div className={S.container}>
-              <div className={S.imgCont}>
-                <img src={el.img} alt="" />
-              </div>
               <div className={S.infoContainer}>
-                <div className={S.stock}>
-                  <label className={S.number}>
-                    {el.status === "active" ? el.stock : el.status}
-                  </label>
-                  <p className={S.units}>{el.stock === 1 ? "Unit" : "Units"}</p>
+                <div className={S.imgCont}>
+                  <img src={el.img} alt="" />
                 </div>
 
                 <div className={S.titleCont}>
                   <span>{el.category.name}</span>
                   <h5>Brand: {el.brand.name}</h5>
-                  <h4>{el.title}</h4>
+                  <h4>
+                    {el.title.length < 60
+                      ? el.title
+                      : el.title.substr(0, 60) + "..."}
+                  </h4>
 
-                  <h6>{el.description}</h6>
-
-                  <div className={S.stockAndStatus}>
-                    <p>Price: ${el.price}</p>
+                  <h6>{el.description.substr(0, 200) + "..."}</h6>
+                  <div className={S.btnContainer}>
+                    <Link to={"/update/" + el.id}>
+                      <button>Update</button>
+                    </Link>
+                    <button onClick={e => handleDelete(el.id)}>Delete</button>
                   </div>
                 </div>
-                <div className={S.btnContainer}>
-                  <Link to={"/update/" + el.id}>
-                    <button>Update</button>
-                  </Link>
-                  <button onClick={e => handleDelete(el.id)}>Delete</button>
+
+                <div className={S.stockAndStatus}>
+                  <div className={S.stock}>
+                    <label className={S.number}>
+                      {el.status === "active" ? el.stock : el.status}
+                    </label>
+                    <p className={S.units}>
+                      {el.stock === 1 ? "Unit" : "Units"}
+                    </p>
+                  </div>
+                  <div className={S.price}>
+                    <p>Price: ${el.price}</p>
+                  </div>
                 </div>
               </div>
             </div>
