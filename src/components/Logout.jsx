8c05b7f7dd623ea7d7715193a810d00fac35e72e
pccomponentshenry <pccomponentshenry@gmail.com>
2023-01-09 00:42:46 -0300

    user banned

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Profile } from "./Profile";
 import L from "../styles/LoginContainer.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../redux/actions";
 
 export const LogoutButton = props => {
   const { logout } = useAuth0();
   const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
 
   return (
     <>
       <div className={L.logoutAndProfile}>
-        <Profile />
+      {user.status === "banned" ? <></>:<Profile />}
         <button
           onClick={() => {
             dispatch(logoutUser());
