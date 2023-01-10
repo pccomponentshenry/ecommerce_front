cd0b82b594001aa2fdcd8e193eabd7477df2a06f
Richard <rsebaburgues@gmail.com>
2023-01-10 21:34:57 +0000

    pequeño avance modo claro oscuro

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,11 @@ import { useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "./Login";
 import { LogoutButton } from "./Logout";
-import mode from "../Images/mode.png";
 import cartImg from "../Images/cart.png";
 import menu from "../Images/menu.png";
+import mode from "../Images/mode.png";
 import N from "../styles/NavBar.module.css";
+
 
 export default function Nav() {
   const [nav, setNav] = useState(false);
@@ -16,106 +17,121 @@ export default function Nav() {
   const cart = useSelector(state => state.cart);
   const { isAuthenticated } = useAuth0();
   const loggedUser = useSelector(state => state.user);
-
   const cartQuantity = cart.reduce((acc, item) => acc + item.quantity, 0);
+ 
 
-  function switchMode() {
-    if (window.scrollY >= 400) {
-      setNav(true);
-    } else {
-      setNav(false);
-    }
+let isDarkMode = false;
+
+function toggleDarkMode(){
+  if (!isDarkMode){
+    //Change to dark
+    document.body.className="lightkmode"
+   
+  } else {
+    //Change to light
+    document.body.className="darkkmode"
   }
+  isDarkMode = !isDarkMode;    
+}
+  // buttonDark.addEventListener("click", toggleDarkMode);
 
-  window.addEventListener("scroll", switchMode);
   return (
     <>
-    {loggedUser.status === "banned" ? <></>:
-      <div
-        className={`${activeNav === true ? N.container : N.containerSmall} ${
-          nav ? N.active : N.container
-        }`}
-      >
+      {loggedUser.status === "banned" ? <></> :
         <div
-          className={N.menuCont}
-          onClick={() => {
-            setActiveNav(!activeNav);
-          }}
+          className={`${activeNav === true ? N.container : N.containerSmall} ${nav ? N.active : N.container
+            }`}
         >
-          <img src={menu} alt="menu icon" />
-        </div>
-        <div className={N.logoAndMenu}>
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <div className={N.logoCont}>
-              <img
-                src="https://res.cloudinary.com/dbtekd33p/image/upload/v1670804226/cqws5x8n/logo_play_expert_wv0yh2.png"
-                alt=""
-                className={N.logo}
-              />
-            </div>
-          </Link>
-          <ul className={N.notResponsiveNav}>
-            <Link to="/sell" style={{ textDecoration: "none" }}>
-              {isAuthenticated && loggedUser.status !== "banned"? <li>Sell</li> : <></>}
+          <div
+            className={N.menuCont}
+            onClick={() => {
+              setActiveNav(!activeNav);
+            }}
+          >
+            <img src={menu} alt="menu icon" />
+          </div>
+          <div className={N.logoAndMenu}>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <div className={N.logoCont}>
+                <img
+                  src="https://res.cloudinary.com/dbtekd33p/image/upload/v1670804226/cqws5x8n/logo_play_expert_wv0yh2.png"
+                  alt=""
+                  className={N.logo}
+                />
+              </div>
             </Link>
-            <Link to="/favorites" style={{ textDecoration: "none" }}>
-              <li>Favorites</li>
-            </Link>
-          </ul>
-          {activeNav === true && (
-            <ul className={isAuthenticated ? N.navList : N.authNavList}>
+            <ul className={N.notResponsiveNav}>
               <Link to="/sell" style={{ textDecoration: "none" }}>
-                {isAuthenticated ? (
+                {isAuthenticated && loggedUser.status !== "banned" ? <li>Sell</li> : <></>}
+              </Link>
+              <Link to="/favorites" style={{ textDecoration: "none" }}>
+                <li>Favorites</li>
+              </Link>
+            </ul>
+            {activeNav === true && (
+              <ul className={isAuthenticated ? N.navList : N.authNavList}>
+                <Link to="/sell" style={{ textDecoration: "none" }}>
+                  {isAuthenticated ? (
+                    <li
+                      className={
+                        isAuthenticated ? N.authenticated : N.notAuthenticated
+                      }
+                    >
+                      Sell
+                    </li>
+                  ) : (
+                    <></>
+                  )}
+                </Link>
+                <Link to="/favorites" style={{ textDecoration: "none" }}>
                   <li
                     className={
                       isAuthenticated ? N.authenticated : N.notAuthenticated
                     }
                   >
-                    Sell
+                    Favorites
                   </li>
-                ) : (
-                  <></>
-                )}
-              </Link>
-              <Link to="/favorites" style={{ textDecoration: "none" }}>
-                <li
-                  className={
-                    isAuthenticated ? N.authenticated : N.notAuthenticated
-                  }
-                >
-                  Favorites
-                </li>
-              </Link>
-            </ul>
-          )}
-        </div>
-        {/* 
+                </Link>
+              </ul>
+            )}
+          </div>
+          {/* 
         <div className={N.searchAndCart}> */}
-        <div className={isAuthenticated ? N.loginText : N.authLoginText}>
-          {isAuthenticated ? (
-            <>
-              <LogoutButton active={activeNav} />
-            </>
-          ) : (
-            <LoginButton active={activeNav} />
-          )}
-        </div>
-        <div className={N.tinyBtns}>
-          {cart.length > 0 && (
-            <span className={N.cartNumber}>{cartQuantity}</span>
-          )}
-          <Link to="/cart">
-            <img src={cartImg} className={N.cart} alt="cart icon" />
-          </Link>
+          <div className={isAuthenticated ? N.loginText : N.authLoginText}>
+            {isAuthenticated ? (
+              <>
+                <LogoutButton active={activeNav} />
+              </>
+            ) : (
+              <LoginButton active={activeNav} />
+            )}
+          </div>
+          <div className={N.tinyBtns}>
+            {cart.length > 0 && (
+              <span className={N.cartNumber}>{cartQuantity}</span>
+            )}
+            <Link to="/cart">
+              <img src={cartImg} className={N.cart} alt="cart icon" />
+            </Link>
 
-          <img
-            src={mode}
-            alt=""
-            className={N.mode}
-            onClick={() => switchMode()}
-          />
-        </div>
-      </div>}
+            {/* <div id="darkmode">
+       
+        <label onClick={toggleDarkMode} htmlFor="checkbox" className="label">
+          <BsMoonStarsFill color="white" />
+          <BsFillSunFill color="yellow" />
+          <div className="ball"></div>
+        </label>
+      </div> */}
+
+            <img
+              src={mode}
+              onClick={toggleDarkMode}
+              alt=""
+              className={N.mode}
+            />
+
+          </div>
+        </div>}
 
       {/* </div> */}
     </>
