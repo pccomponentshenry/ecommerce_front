90134b77ad3f82c1a1293e3ac66bb0617a5bf2eb
SantiCalabro <santi.calabro93@gmail.com>
2023-01-12 11:42:28 -0300

    ajustes

--- src/components/DetailCarousel.jsx
+++ src/components/DetailCarousel.jsx
@@ -1,14 +1,12 @@
 import Carousel from "react-bootstrap/Carousel";
 import D from "../styles/DetailCarousel.module.css";
-import { useSelector } from "react-redux";
 
 function UncontrolledExample(props) {
-  const product = useSelector(state => state.product);
   return (
     <div className={D.CarouselCont}>
       <Carousel variant="dark">
-        {product.img &&
-          product.img.map((el, i) => (
+        {props.img.length > 0 &&
+          props.img.map((el, i) => (
             <Carousel.Item key={i}>
               <div className={D.imgCont}>
                 <img className={D.image} src={el} alt="First slide" />
