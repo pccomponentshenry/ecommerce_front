5611cbb62fa9b553f7d1454f49259a0817986085
SantiCalabro <santi.calabro93@gmail.com>
2023-01-25 14:16:04 -0300

    ajustes de estilos

--- src/components/DashboardPurchase.jsx
+++ src/components/DashboardPurchase.jsx
@@ -10,45 +10,56 @@ export default function Purchases() {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
-  
-console.log(params)
+
+  console.log(params);
   useEffect(() => {
     dispatch(getDetailsOrders(params.id));
-  }, [])
+  }, []);
 
   return (
     <div>
-      {purchases.map(p => (
-        <div className={P.orderBox} key={p.id}>
-          <h4>
-            Order PE000{p.id}
-          </h4>
-          <span className={`${p.status === "completed" ? P.completed : {}} ${p.status === "cancelled" ? P.cancelled : {}}`}>
-            {p.status.charAt(0).toUpperCase() + p.status.slice(1)}
-          </span>
-          <h6 className={`${p.status === "completed" ? P.dateCompleted : {}} ${p.status === "cancelled" ? P.dateCancelled : {}}`}>
-            {p.status === "completed" ? "Purchase Date:" : "Date:"} {p.purchaseDate.substr(0, 10)}
-          </h6>
-          {p.orderItems.map(el => (
-            <div className={P.orderCard} key={el.id}>
-              <div className={P.imageCont}>
-                <img src={el.product.img} alt="" />
-              </div>
-              <div className={P.cardInfo}>
-                <Link
-                  style={{ textDecoration: "none", color: "#212121" }}
-                  to={`/detail/${el.product.id}`}
-                >
-                  <h3>{el.product.title}</h3>
-                </Link>
-                <h5>
-                  {el.quantity} Units - ${el.price}
-                </h5>
-              </div>
+      <div className={P.container}>
+        {purchases.map(p => (
+          <div className={P.orderBox} key={p.id}>
+            <div className={P.headline}>
+              <h4>Order PE000{p.id}</h4>
+              <span
+                className={`${p.status === "completed" ? P.completed : {}} ${
+                  p.status === "cancelled" ? P.cancelled : {}
+                }`}
+              >
+                {p.status.charAt(0).toUpperCase() + p.status.slice(1)}
+              </span>
+              <h6
+                className={`${
+                  p.status === "completed" ? P.dateCompleted : {}
+                } ${p.status === "cancelled" ? P.dateCancelled : {}}`}
+              >
+                {p.status === "completed" ? "Purchase Date:" : "Date:"}{" "}
+                {p.purchaseDate.substr(0, 10)}
+              </h6>
             </div>
-          ))}
-        </div>
-      ))}
+            {p.orderItems.map(el => (
+              <div className={P.orderCard} key={el.id}>
+                <div className={P.imageCont}>
+                  <img src={el.product.img} alt="" />
+                </div>
+                <div className={P.cardInfo}>
+                  <Link
+                    style={{ textDecoration: "none", color: "#212121" }}
+                    to={`/detail/${el.product.id}`}
+                  >
+                    <h3>{el.product.title}</h3>
+                  </Link>
+                  <h5>
+                    {el.quantity} Units - ${el.price}
+                  </h5>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
